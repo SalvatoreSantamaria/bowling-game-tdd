592873cd263760ed2c8b1c540523a16333f8b94a
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-11-17 05:58:57 -0800

    Basic test to score 0 added

--- bowling.rb
+++ bowling.rb
@@ -1,3 +1,11 @@
 class Bowling
+
+  def roll(pins) 
+    
+  end
+
+  def score
+    return 0
+  end
   
 end
--- bowlingSpec.rb
+++ bowlingSpec.rb
@@ -5,4 +5,11 @@ describe 'Bowling Game' do
   it('Can make a new game') do
     game = Bowling.new
   end
+
+  it ('Can have a game with the score of 0') do
+    game = Bowling.new
+    20.times{game.roll(0)}
+    expect(game.score).to eq(0)
+  end
+
 end
