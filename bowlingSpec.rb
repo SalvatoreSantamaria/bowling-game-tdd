993c6f846fd83f9df1a13dd1867e8637e00f388e
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-11-17 07:34:33 -0800

    Added before do to clean up tests

--- bowlingSpec.rb
+++ bowlingSpec.rb
@@ -2,44 +2,43 @@ require 'rspec'
 require_relative 'bowling'
 
 describe 'Bowling Game' do 
+
+  before do
+    @game = Bowling.new
+  end
+
   it('Can make a new game') do
-    game = Bowling.new
   end
 
   it ('Can have a game with the score of 0') do
-    game = Bowling.new
-    20.times{game.roll(0)}
-    expect(game.score).to eq(0)
+    20.times{@game.roll(0)}
+    expect(@game.score).to eq(0)
   end
 
   it ('Can have a game where each roll was 1') do
-    game = Bowling.new
-    20.times{game.roll(1)}
-    expect(game.score).to eq(20)
+    20.times{@game.roll(1)}
+    expect(@game.score).to eq(20)
   end
 
   it ('Can roll a spare') do
-    game = Bowling.new
-    game.roll(5)
-    game.roll(5)
-    game.roll(3)
-    20.times{game.roll(0)}
-    expect(game.score).to eq(16)
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
+    20.times{@game.roll(0)}
+    expect(@game.score).to eq(16)
   end
 
   it ('Can roll a strike') do
-    game = Bowling.new
-    game.roll(10)
-    game.roll(3)
-    game.roll(4)
-    16.times{game.roll(0)}
-    expect(game.score).to eq(24)
+    @game.roll(10)
+    @game.roll(3)
+    @game.roll(4)
+    16.times{@game.roll(0)}
+    expect(@game.score).to eq(24)
   end
 
   it ('Can have a game with the score of 300') do
-    game = Bowling.new
-    12.times{game.roll(10)}
-    expect(game.score).to eq(300)
+    12.times{@game.roll(10)}
+    expect(@game.score).to eq(300)
   end
 
 end
